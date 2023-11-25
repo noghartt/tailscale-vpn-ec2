69640b51959adf8cf6407863fe9585b421214e97
Guilherme Ananias <hi@noghartt.dev>
2023-11-25 17:58:57 -0300

    setup initial cdk

--- bin/tailscale-vpn-ec2.ts
+++ bin/tailscale-vpn-ec2.ts
@@ -2,20 +2,21 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TailscaleVpnEc2Stack } from '../lib/tailscale-vpn-ec2-stack';
+import 'dotenv/config';
 
 const app = new cdk.App();
-new TailscaleVpnEc2Stack(app, 'TailscaleVpnEc2Stack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+type StackPerRegionArgs = {
+  id: string;
+  region: string;
+  node: string;
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const stackPerRegion = ({ id, region, node }: StackPerRegionArgs) =>
+  new TailscaleVpnEc2Stack(app, id, {
+    env: { region },
+    tailscaleAuthKey: process.env.TAILSCALE_AUTH_KEY!,
+    exitNodeName: node,
+  });
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+stackPerRegion({ id: 'ExitNodeNorthVirginia', region: 'us-east-1', node: 'TSNorthVirginiaExitNode' });
--- lib/tailscale-vpn-ec2-stack.ts
+++ lib/tailscale-vpn-ec2-stack.ts
@@ -1,16 +1,55 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CloudFormationInit, InitCommand, InitFile, Instance, InstanceType, IpAddresses, MachineImage, OperatingSystemType, SubnetType, UserData, Vpc } from 'aws-cdk-lib/aws-ec2';
+
+interface ExitNodeProps extends cdk.StackProps {
+  tailscaleAuthKey: string,
+  exitNodeName: string
+}
 
 export class TailscaleVpnEc2Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ExitNodeProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'ExitNodeVPC', {
+      ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'tailscale',
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TailscaleVpnEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const publicSubnets = vpc.selectSubnets({
+      subnetType: SubnetType.PUBLIC,
+    });
+
+    const machineImage = MachineImage.fromSsmParameter(
+      '/aws/service/canonical/ubuntu/server/jammy/stable/current/amd64/hvm/ebs-gp2/ami-id',
+    );
+
+    const userData = UserData.forLinux();
+    userData.addCommands(
+      // Enable ip forwarding 
+      "echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf",
+      "echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf",
+      "sysctl -p /etc/sysctl.conf",
+
+      // Install tailscale
+      "curl -fsSL https://tailscale.com/install.sh | sh",
+      `tailscale up --authkey ${props.tailscaleAuthKey} --advertise-exit-node --hostname=${props.exitNodeName}`
+    );
+
+    const instance = new Instance(this, "exitNode", {
+      instanceType: new InstanceType("t3.micro"),
+      vpc,
+      instanceName: props.exitNodeName,
+      vpcSubnets: publicSubnets,
+      machineImage,
+      userData,
+    });
   }
 }
