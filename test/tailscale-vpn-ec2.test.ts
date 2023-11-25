5a5185ed1084ada20a2a6bdacdefb55c23b8366c
Guilherme Ananias <hi@noghartt.dev>
2023-11-25 15:25:41 -0300

    initial commit

---
+++ test/tailscale-vpn-ec2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TailscaleVpnEc2 from '../lib/tailscale-vpn-ec2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tailscale-vpn-ec2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TailscaleVpnEc2.TailscaleVpnEc2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
